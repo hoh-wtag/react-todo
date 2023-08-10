4471f938f8b0d5ff0b430bc4dc976ac6d95655af
Hasibul Hoque Chowdhury <hasibul.hoque@welldev.io>
2023-08-10 10:58:19 +0600

    Updated id

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,7 +3,7 @@ import { ADD_TASK } from "@store/types";
 export const addTask = (title) => ({
   type: ADD_TASK,
   payload: {
-    id: Date.now(),
+    id: Date.now().toString(),
     title: title,
     createdDate: new Date(),
   },
